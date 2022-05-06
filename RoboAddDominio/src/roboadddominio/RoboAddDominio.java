8afb855cf7c82178471a4e6d3ffd6441a0822fa0
Jota02 <jota02@jota.local>
2022-05-06 10:16:42 -0300

    0605

--- RoboAddDominio/src/roboadddominio/RoboAddDominio.java
+++ RoboAddDominio/src/roboadddominio/RoboAddDominio.java
@@ -13,18 +13,47 @@ public class RoboAddDominio {
 
     public static void main(String[] args) throws AWTException {
         Robot r = new Robot();
-        r.setAutoDelay(2000);
-        /*r.mouseMove(-800, 510); //JANELA  DOMINIO
-        r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-        r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        for (int i = 0; i<225; i++){
+        r.setAutoDelay(150);               
         r.mouseMove(1000, 10);  //EXCEL
         r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        r.delay(200);
+        r.keyPress(KeyEvent.VK_CONTROL);
+        r.keyPress(KeyEvent.VK_C);
+        r.keyRelease(KeyEvent.VK_CONTROL);
+        r.keyRelease(KeyEvent.VK_C);
+        r.delay(200);
+        r.mouseMove(-800, 510); //JANELA  DOMINIO novo
+        r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        r.keyPress(KeyEvent.VK_CONTROL);
+        r.keyPress(KeyEvent.VK_V);
+        r.delay(200);
+        r.keyRelease(KeyEvent.VK_CONTROL);
+        r.keyRelease(KeyEvent.VK_V);
+        r.mouseMove(1000, 10);  //EXCEL
+        r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        r.delay(200);
+        r.keyPress(KeyEvent.VK_TAB);
+        r.keyRelease(KeyEvent.VK_TAB);
+        r.delay(200);        
+        r.mouseMove(300, 250);
+        r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        r.mouseMove(255, 175);
         r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
         r.keyPress(KeyEvent.VK_CONTROL);
         r.keyPress(KeyEvent.VK_C);
         r.keyRelease(KeyEvent.VK_CONTROL);
         r.keyRelease(KeyEvent.VK_C);
+        r.delay(200);
         r.mouseMove(-800, 450);
+        r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+        r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+        r.keyPress(KeyEvent.VK_TAB);
+        r.keyRelease(KeyEvent.VK_TAB);
+        r.delay(200);
         r.keyPress(KeyEvent.VK_CONTROL);
         r.keyPress(KeyEvent.VK_V);
         r.keyRelease(KeyEvent.VK_CONTROL);
@@ -32,28 +61,12 @@ public class RoboAddDominio {
         r.mouseMove(1000, 10);  //EXCEL
         r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
         r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-        r.keyPress(KeyEvent.VK_TAB);
-        r.keyRelease(KeyEvent.VK_TAB);
-        r.keyPress(KeyEvent.VK_CONTROL);
-        r.keyPress(KeyEvent.VK_C);
-        r.keyRelease(KeyEvent.VK_CONTROL);
-        r.keyRelease(KeyEvent.VK_C);
-        r.mouseMove(-800, 450);
-        r.keyPress(KeyEvent.VK_TAB);
-        r.keyRelease(KeyEvent.VK_TAB);
-        r.keyPress(KeyEvent.VK_CONTROL);
-        r.keyPress(KeyEvent.VK_V);
-        r.keyRelease(KeyEvent.VK_CONTROL);
-        r.keyRelease(KeyEvent.VK_V);*/
-        r.mouseMove(300, 250);r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-        r.mouseMove(255, 175);
-        r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-        r.keyPress(KeyEvent.VK_CONTROL);
-        r.keyPress(KeyEvent.VK_C);
-        r.keyRelease(KeyEvent.VK_CONTROL);
-        r.keyRelease(KeyEvent.VK_C);
+        r.keyPress(KeyEvent.VK_ESCAPE);
+        r.keyPress(KeyEvent.VK_DOWN);
+        r.keyPress(KeyEvent.VK_LEFT);
         
-        
+        }    
+       
     }
     
 }
